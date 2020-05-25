63e79cc79fec86da139891a64456cb4f0cd5fadf
Linjason0209 <linjason0209@gmail.com>
2020-05-26 01:17:11 +0800

    Home PageUI setting

---
+++ javascriptPractice/ResultPractice/public/javascripts/Screen.js
@@ -0,0 +1,20 @@
+let x = 10;//預設圓的訊息
+let y = 10;
+let r = 5;
+let c = "#000000";
+console.log("Sent");
+function setup() {
+    createCanvas(400,400);//給予畫面畫布
+    io().on("Screen",function (message) {//接收來自Server.js的emit資料
+        console.log("Screen.js"+message);//browser端印出
+        x = message.x;//更改預設為client端數值
+        y = message.y;
+        r = message.r;
+        c = message.c;
+    });
+}
+
+function draw() {
+    stroke(c);//填滿顏色
+    ellipse(x,y,r);//畫出圓形
+}
